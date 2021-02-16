4fd8d435615e350c6e794740a2bdfe5e0ebeb8fc
J.Henimex <J.Henimex@IT-01>
2021-02-16 16:11:29 +0300

    Caching

---
+++ HenDevFramework.Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace HenDevFramework.Core.CrossCuttingConcerns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        protected ObjectCache Cache
+        {
+            get
+            {
+                return MemoryCache.Default;
+            }
+        }
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string key, object data, int cacheTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAdd(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
